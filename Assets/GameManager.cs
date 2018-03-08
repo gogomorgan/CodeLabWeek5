ff7ab96f6dff51834d8a7f186bee21a649c2e832
gogomorgan <macrimorganb@gmail.com>
2018-03-08 16:12:15 -0500

    Unity Stuff

---
+++ Assets/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject PlayerPrefab;
+    public GameObject FirePrefab;
+    public GameObject TreasurePrefab;
+
+    public float tileWidth = 2f;
+    public float tileHeight = 2f;
+
+    public string levelFile = "level1.txt";
+
+    // Use this for initialization
+    void Start()
+    {
+
+        string levelString = File.ReadAllText(Application.dataPath + Path.DirectorySeparatorChar + levelFile);
+
+        string[] levelLines = levelString.Split('\n');
+        int width = 0;
+
+        for (int row = 0; row < levelLines.Length; row++)
+        {
+            string currentLine = levelLines[row];
+            width = currentLine.Length;
+
+            for (int col = 0; col < currentLine.Length; col++)
+            {
+                char currentChar = currentLine[col];
+                if (currentChar == 'x')
+                {
+                    GameObject FireObj = Instantiate(FirePrefab);
+                    FireObj.transform.parent = transform;
+                    FireObj.transform.position = new Vector3(col*tileWidth, -row*tileHeight, 0);
+                    
+                }
+
+                else if (currentChar == 'p')
+                {
+                    GameObject PlayerObj = Instantiate(PlayerPrefab);
+                    PlayerObj.transform.parent = transform;
+                    PlayerObj.transform.position = new Vector3(col*tileWidth, -row*tileHeight, 0);
+                }
+
+                else if (currentChar == 'e')
+                {
+                    GameObject TreasureObj = Instantiate(TreasurePrefab);
+                    TreasureObj.transform.parent = transform;
+                    TreasureObj.transform.position = new Vector3(col*tileWidth, -row*tileHeight, 0);
+
+                }
+            }
+        }
+
+        float myX = -(width * tileWidth) / 2f + tileWidth / 2f;
+        float myY = (levelLines.Length * tileHeight) / 2f - tileHeight / 2f;
+        transform.position = new Vector3(myX, myY, 0);
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>(); 
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Treasure"))
+        {
+            Debug.Log("win!");
+        }
+    }
+}
